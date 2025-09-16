592ddd2e2e83a760609121babb923f048fe15dec
zphrs <z@zephiris.dev>
2025-09-16 15:54:38 -0700

    delete target

--- sky/target/rust-analyzer/debug/deps/sky-808a2b55a4d525bd.d
+++
@@ -1,5 +0,0 @@
-/Users/zphrs/Documents/programming/plexigraph/hermes/sky/target/rust-analyzer/debug/deps/sky-808a2b55a4d525bd.d: src/lib.rs
-
-/Users/zphrs/Documents/programming/plexigraph/hermes/sky/target/rust-analyzer/debug/deps/libsky-808a2b55a4d525bd.rmeta: src/lib.rs
-
-src/lib.rs:
