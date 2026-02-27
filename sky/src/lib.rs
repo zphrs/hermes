de907a82b80124a3e87cea8a0b5d416a131181ea
zphrs <z@zephiris.dev>
2026-02-26 18:14:40 -0800

    cleaned up sky a bit

--- sky/src/lib.rs
+++ sky/src/lib.rs
@@ -1,9 +1,2 @@
-#![feature(ip_as_octets)]
-#![feature(iter_array_chunks)]
-
-mod capnp_types;
 mod listener;
 pub mod node;
-pub mod server;
-
-capnp::generated_code!(mod sky_capnp);
--- sky/src/node.rs
+++ sky/src/node.rs
@@ -1,4 +1,4 @@
-use std::net::{IpAddr, SocketAddr};
+use std::net::{SocketAddr};
 
 use kademlia::{HasId, Id};
 use sha2::{Digest, Sha256};
