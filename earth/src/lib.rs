dfe5470225e7899a06df475fe028813c706470d7
Zephiris Evergreen <z@zephiris.dev>
2026-08-19 12:45:51 -0400

    cleanup unused dependencies; specify used dependencies

--- earth/src/lib.rs
+++ earth/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use shared_schema::EarthNode;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait Sky {
+    fn nearby_earth_nodes() -> impl std::future::Future<Output = EarthNode>;
 }
